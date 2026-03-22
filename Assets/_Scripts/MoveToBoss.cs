43b40f98cb2f08ccff934354afccbca0cffe0f51
Michalis Christodoulou <michchristo@gmail.com>
2026-03-22 13:14:03 +0200

    add enemy characters prefabs with animations and navmesh navigation to chase player

---
+++ Assets/_Scripts/MoveToBoss.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveToBoss : MonoBehaviour
+{
+    [SerializeField] private Transform target; // Joe
+
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (target == null) return;
+
+        // Continuously update destination to player's position
+        agent.SetDestination(target.position);
+
+        float speed = agent.velocity.magnitude;
+        animator.SetFloat("Speed", speed);
+    }
+}
